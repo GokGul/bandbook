7c7f91119d486bd430ac40288dac72ca829faecc
gokguu <gokhan_g32@yahoo.com>
2017-03-21 17:55:31 +0100

    Initial commit

---
+++ Bandbook/Bandbook/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bandbook.MvcApplication" Language="C#" %>
---
+++ Bandbook/Bandbook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bandbook.Startup))]
+namespace Bandbook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
