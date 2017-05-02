87930a03fbba915dc350bcfa3e5a6f09b29b94a6
gokguu <gokhan_g32@yahoo.com>
2017-05-02 14:37:36 +0200

    Created create gig page and viewmodel.

---
+++ Bandbook/Bandbook/Controllers/GigController.cs
@@ -0,0 +1,25 @@
+﻿using Bandbook.Models;
+using Bandbook.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Bandbook.Controllers
+{
+    public class GigController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        public GigController()
+        {
+            context = new ApplicationDbContext();
+        }
+        // GET: Gig
+        public ActionResult Create()
+        {
+            var viewModel = new BandFormViewModel
+            {
+                Genres = context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ Bandbook/Bandbook/ViewModels/BandFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Bandbook.Models;
+using System.Collections.Generic;
+
+namespace Bandbook.ViewModels
+{
+    public class BandFormViewModel
+    {
+        public string Place { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+
+    }
+}
